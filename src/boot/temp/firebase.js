e734ae1afc26548fefe2130b94a41f23fe287c4f
ahayworth1 <93269307+ahayworth1@users.noreply.github.com>
2023-05-09 08:02:08 -0500

    Update firebase.js

--- src/boot/temp/firebase.js
+++ src/boot/temp/firebase.js
@@ -1,17 +1,19 @@
 import firebase from 'firebase/app'
-import "firebase/firestore"
+import 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA_8k9ItknEXlXYzoRhwaOHnY92MzYdAOg",
-  authDomain: "quitter2-b5767.firebaseapp.com",
-  projectId: "quitter2-b5767",
-  storageBucket: "quitter2-b5767.appspot.com",
-  messagingSenderId: "149060933522",
-  appId: "1:149060933522:web:2094c612f552559e47d34b"
-}
+  apiKey: "AIzaSyDIHEd24KagMbzmIL3OqK_gd0oeRreNtEc",
+  authDomain: "quitter2-5daf3.firebaseapp.com",
+  projectId: "quitter2-5daf3",
+  storageBucket: "quitter2-5daf3.appspot.com",
+  messagingSenderId: "1071170439591",
+  appId: "1:1071170439591:web:10c37782a7cc1a1d818e82"
+};
 
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig)
 
-let db = firebase.firestore()
+// access firebase database with db variable
+const db = firebase.firestore()
 
-export default db 
+export default db
